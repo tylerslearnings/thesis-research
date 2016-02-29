3ab7df06730ef631026b25dced2c2b122ad21e6e
mtreeves1990 <mreeves90@cox.net>
2016-02-28 20:42:20 -0700

    Dividend Data

--- QuantitativeAnalyzer.py
+++ QuantitativeAnalyzer.py
@@ -6,6 +6,7 @@ from yahoo_finance import Share
 from TechnicalAnalyzer import TechnicalAnalyzer
 import urllib2
 import datetime
+import csv
 
 
 REVENUE = 'revenue'
@@ -46,8 +47,8 @@ def ExtractTablesAndRows(url):
     MINYEAR = 2007
     rows = []
     dateStore = []
-    date = datetime.datetime.strptime('13 Feb 2015', '%d %b %Y')
-    stopDate = datetime.datetime.strptime('13 Feb 2015', '%d %b %Y')
+    date = datetime.datetime.strptime('26 Mar 2015', '%d %b %Y')
+    stopDate = datetime.datetime.strptime('26 Mar 2015', '%d %b %Y')
     currentUrl = url
     i = 1
     while date.year > MINYEAR:
@@ -210,14 +211,21 @@ def ExtractStockMarketData(associatedDates, companyName):
     stockGrowth = []
     marketGrowth = []
     yearGrowth = []
+    dividends = []
+
+    allDays = None
+
+    url = urlBuilder(associatedDates, shr.symbol)
+
+
+    response = urllib2.urlopen(url)
+    reader = csv.reader(response)
+    cr = list(reader)
 
     for date in associatedDates:
         currentDate = date.strftime("%Y-%m-%d")
         pastDate = date.replace(year = date.year - 1).strftime("%Y-%m-%d")
         futureDate = date.replace(year = date.year + 1).strftime("%Y-%m-%d")
-
-        print currentDate
-        print pastDate
 
         allDays = shr.get_historical(pastDate, currentDate)
         allsp = sp500.get_historical(pastDate, currentDate)
@@ -230,15 +238,23 @@ def ExtractStockMarketData(associatedDates, companyName):
         firstSP500 = allsp[0]
         secondSP500 = allsp[-1]
 
-        print lastYearPrice['Close']
-        print firstYearPrice['Close']
 
-        print firstSP500['Close']
-        print secondSP500['Close']
+        dividendGrowth = 0
+        pastGrowth = 0
+        change = False
+        growthS = getGrowth(float(lastYearPrice['Close']), float(firstYearPrice['Close']))
+        growthM = getGrowth(float(secondSP500['Close']), float(firstSP500['Close']))
+        growthY = getGrowth(float(firstYearPrice['Close']), float(futurePrice['Close']))
 
-        stockGrowth.append(getGrowth(float(lastYearPrice['Close']), float(firstYearPrice['Close'])))
-        marketGrowth.append(getGrowth(float(secondSP500['Close']), float(firstSP500['Close'])))
-        yearGrowth.append(getGrowth(float(firstYearPrice['Close']), float(futurePrice['Close'])))
+        print growthS
+        print growthM
+        print growthY
+        stockGrowth.append(growthS)
+        marketGrowth.append(growthM)
+        yearGrowth.append(growthY)
+
+    dividends = dividenFind(associatedDates, cr[1:len(cr) - 1])
+
 
 def getShare(companyName):
     with open("tickers.txt") as companies:
@@ -254,3 +270,83 @@ def getGrowth(firstYearPrice, secondYearPrice):
     growth = difference/firstYearPrice
     percentGrowth = growth
     return percentGrowth
+
+def urlBuilder(associatedDates, share):
+    url = 'http://ichart.yahoo.com/table.csv?'
+
+    url = url + 's=' + share + '&c='
+
+    firstYear = associatedDates[-1].year
+    lastYear = associatedDates[0].year
+
+    url = url + str(firstYear) + '&a='
+
+    firstMonth = associatedDates[-1].month
+    lastMonth = associatedDates[0].month
+
+    url = url + str(firstMonth) + '&b='
+
+    firstDay = associatedDates[-1].day
+    lastDay = associatedDates[0].day
+
+    url = url + str(firstDay) + '&f=' + str(lastYear) + '&d=' + str(lastMonth) + '&e=' + str(lastDay) + '&g=v&ignore=.csv'
+
+    return url
+
+def dividenFind(associatedDates, dividenList):
+    dividendTrend = []
+    for i in range(0, len(associatedDates)):
+        currentChange = False
+
+        currentDate = associatedDates[i]
+        compareDate = datetime.datetime.strptime(dividenList[0][0], "%Y-%m-%d")
+        min = abs(currentDate - compareDate)
+        currentIndex = 0
+        for j in range(1, len(dividenList)):
+
+            compareDate = datetime.datetime.strptime(dividenList[j][0], "%Y-%m-%d")
+
+            newDiff = abs(currentDate - compareDate)
+            if newDiff.days < min.days:
+                min = newDiff
+                currentIndex = j
+                currentChange = True
+            else:
+                break
+        if currentChange == False:
+            dividendTrend.append(0.0)
+
+        else:
+            pastChange = False
+            pastDate = associatedDates[i].replace(year=associatedDates[i].year - 1)
+            compareDate = datetime.datetime.strptime(dividenList[0][0], "%Y-%m-%d")
+            min = abs(pastDate - compareDate)
+            pastIndex = 0
+
+            for j in range(1, len(dividenList)):
+
+                compareDate = datetime.datetime.strptime(dividenList[j][0], "%Y-%m-%d")
+                newDiff = abs(pastDate - compareDate)
+
+                if newDiff.days < min.days:
+                    min = newDiff
+                    pastIndex = j
+                    pastChange = True
+                else:
+                    break
+
+
+            if pastChange == True:
+
+                pastDividend = float(dividenList[pastIndex][1])
+                currentDividend = float(dividenList[currentIndex][1])
+                trend = (currentDividend - pastDividend) / pastDividend
+
+                dividendTrend.append(trend)
+
+
+            else:
+                currentDividend = float(dividenList[currentIndex][1])
+                dividendTrend.append(currentDividend*100)
+
+    return dividendTrend
